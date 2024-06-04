0eda13521e5b865f3c988ca61741521fb36295da
Cocotte123 <rousseauemmanuelle1@yahoo.fr>
2024-06-04 17:15:33 +0200

    search

--- src/components/ListeArtisans.jsx
+++ src/components/ListeArtisans.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import '../components/ListeArtisans.css';
 /*import Artisans from '../datas/datas.json';*/
 import { ListeContext } from "../context/ListeContext";
@@ -8,30 +8,34 @@ import { Link } from "react-router-dom";
 
 const ListeArtisans = (props) => {
     const {Artisans} = useContext(ListeContext);
+    
 
     return(
         <div>
             <h1>{props.category}</h1>
+            <p>{props.queryArtisan}</p>
             <div>
-                {Artisans.map((artisan)=>{
-                    if(props.category===artisan.category){
-                        return(
+                {Artisans.filter(artisan=>artisan.name.toLowerCase().includes(props.queryArtisan.toLowerCase()) ||
+                        artisan.specialty.toLowerCase().includes(props.queryArtisan.toLowerCase()) ||
+                        artisan.location.toLowerCase().includes(props.queryArtisan.toLowerCase()) ).map((artisan)=>{
+                    if(props.category===artisan.category)
+                    {
+                       return(
                             <Link to={`/artisan/${artisan.id}`} key={artisan.id}>
                             {artisan.name}
                             </Link>
                         )
-                    }
-                    else {
+                    } 
+                    else 
+                    {
                         return null;
                     }
-                }
-
-                )
-
-                }
+                    }
+                    )
+                   }
             </div>
         </div>
-    )
+    );
 }
 
 export default ListeArtisans;
